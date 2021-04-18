9d1fbcb030d0215ec8a783368340974bdc915c38
mpriyamsa <mpriyamsa@gmail.com>
2021-04-17 20:47:14 -0400

    Add files via upload

---
+++ Plasmidmap.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Plasmidmap:
+
+    def __init__(self, my_dataframe, api_key):
+        self.my_dataframe = my_dataframe
+        self.api_key = api_key
+
+    def get_location(self):
+        latitudes = []
+        longitudes = []
+        # For each address in data frame get latitude and longitude
+        for elm in self.my_dataframe['Address']:
+            geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address={}".format(elm) + "&key={}".format(
+                self.api_key)
+            r = requests.get(geocode_url)
+            results = r.json()['results']
+            latitudes.append(results[0].get('geometry').get('location').get('lat'))
+            longitudes.append(results[0].get('geometry').get('location').get('lng'))
+        self.my_dataframe['Latitude'] = latitudes
+        self.my_dataframe['Longitude'] = longitudes
+        print(self.my_dataframe)
+        # Write latitude and longitude data to csv
+        self.my_dataframe.to_csv('New_Long_Lats.csv', index=False, mode='a', header=False)
+
+
+
---
+++ plotting.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from shapely.geometry import Point
+import geopandas as gpd
+from geopandas import GeoDataFrame
+import matplotlib.pyplot as plt
+import random
+
+# United States
+US_LAT_HIGH = 50.862137
+US_LAT_LOW = 26.903456
+US_LON_HIGH = -123.708449
+US_LON_LOW = -59.548298
+
+# Europe
+EU_LAT_HIGH = 71.982614
+EU_LAT_LOW = 34.931584
+EU_LON_HIGH = -19.171315
+EU_LON_LOW = 52.371647
+
+# Asia
+AS_LAT_HIGH = 50.324893
+AS_LAT_LOW = 5.496438
+AS_LON_HIGH = 95.789622
+AS_LON_LOW = 148.348211
+
+
+# Get api-key
+api_file = open("api-key.txt", "r")
+api_key = api_file.read()
+api_file.close()
+
+# Read data and connect with Google maps API to get latitude and longitude
+df_plasmids = pd.read_excel(r'Perli_plasmids_Still_to_be_drawn.xlsx', header=None, names=['ID', 'Professor', 'Institute', 'Year'])
+df_plasmids['Address'] = df_plasmids['Professor'] + ', ' + df_plasmids['Institute']
+
+df = pd.read_csv('New_Long_Lats.csv')
+
+for i in range(len(df)):
+    df['Latitude'][i] = (df['Latitude'][i] + random.uniform(0.1, 1)).copy()
+    df['Longitude'][i] = (df['Longitude'][i] + random.uniform(0.1, 1)).copy()
+
+# Reading coordinates for Asia
+df = df[(df['Latitude'] < AS_LAT_HIGH) & (df['Latitude'] > AS_LAT_LOW)]
+df = df[(df['Longitude'] < AS_LON_LOW) & (df['Longitude'] > AS_LON_HIGH)]
+AS = df.ID.count()
+
+geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]
+gdf = GeoDataFrame(df, geometry=geometry)
+
+# This is a simple world map that goes with geopandas
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# Plot only USA
+# USA = world.cx[US_LAT_LOW:US_LAT_HIGH, US_LON_LOW:US_LON_HIGH]
+
+# We remove Antarctica.
+ax = world[world.name != 'Antarctica'].plot(figsize=(30, 10), color='#F9E076')
+ax.axis('off')
+
+# We can now plot our ``GeoDataFrame``.
+gdf.plot(ax=ax, marker='o', color='red', markersize=25)
+
+plt.show()
+
